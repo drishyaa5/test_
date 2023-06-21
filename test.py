f31c082841daef5ca3e369847bf4a866ff93b210
Drishya Adhikari <drishyaa005@gmail.com>
2023-06-21 08:10:23 +0545

    added test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+
+video = YouTube(link)
+
+print("Video Title: ", video.title)
+print("Video Views: ", video.views)
+
+dl = video.streams.get_highest_resolution()
+dl.download("./ytdownload")
